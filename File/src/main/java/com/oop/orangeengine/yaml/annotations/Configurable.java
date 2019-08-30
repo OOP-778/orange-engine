699e2f2a299066cb1846d219edcd52ace40bf8dc
Povilas <oskardhavel@gmail.com>
2019-08-30 16:58:36 +0300

    Configuration Update
    Took 6 minutes

--- File/src/main/java/com/oop/orangeengine/yaml/annotations/Configurable.java
+++ File/src/main/java/com/oop/orangeengine/yaml/annotations/Configurable.java
@@ -10,104 +10,213 @@ import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.List;
 
+import static com.oop.orangeengine.main.Engine.getEngine;
+
 public interface Configurable {
 
-    default void initConfig(OConfiguration configuration) throws ClassNotFoundException, IllegalAccessException {
+    default void load(ConfigurationSection section) {
+        try {
+            for (Field field : getClass().getDeclaredFields()) {
 
-        for (Field field : getClass().getDeclaredFields()) {
+                field.setAccessible(true);
 
-            field.setAccessible(true);
+                for (Annotation annotation : field.getDeclaredAnnotations()) {
 
-            for (Annotation annotation : field.getDeclaredAnnotations()) {
+                    Class reqProductType = null;
 
-                Class reqProductType = null;
+                    if (annotation instanceof ConfigValue) {
 
-                if (annotation instanceof ConfigValue) {
+                        ConfigValue configValue = (ConfigValue) annotation;
 
-                    ConfigValue configValue = (ConfigValue) annotation;
-
-                    //Check if getValue exists
-                    if (!configuration.isPresentValue(configValue.path()))
-                        throw new IllegalStateException("Failed to find configuration getValue at (" + configValue.path() + ") at file (" + configuration.getOFile().getFile().getName() + ")");
-
-                    reqProductType = field.getType();
-                    Object value = configuration.getValue(configValue.path()).getValue();
-
-                    //Check if configuration getValue is parsable to field type
-                    if (value.getClass().isInstance(reqProductType))
-                        throw new IllegalStateException("Failed to parse configuration getValue at (" + configValue.path() + ") at file (" + configuration.getOFile().getFile().getName() + ") as " + reqProductType.getTypeName());
-
-                    //Set the field
-                    field.set(this, value);
-
-                } else if (annotation instanceof SectionValue) {
-
-                    SectionValue sectionValue = (SectionValue) annotation;
-
-                    //We have to find out if the getSection at given path even exists
-                    if (!configuration.isPresentSection(sectionValue.path()))
-                        throw new IllegalStateException("Failed to find configuration getSection at (" + sectionValue.path() + ") at file (" + configuration.getOFile().getFile().getName() + ")");
-
-                    //Requires a list of getValues, else it is a single getValue
-                    if (field.getGenericType() instanceof ParameterizedType) {
-
-                        //Check if field getValue is initialized
-                        if (field.get(this) == null)
-                            throw new IllegalStateException("The field list should be initialized in order for configuration to init!");
-
-                        ParameterizedType parameterizedType = (ParameterizedType) field.getGenericType();
-                        for (Type t : parameterizedType.getActualTypeArguments()) {
-
-                            reqProductType = Class.forName(t.getTypeName());
-                            break;
-
-                        }
-
-                        if (reqProductType == null) continue;
-
-                        //Because requirement class is ready to be used
-                        //It's time to look for getSection mapper for required class
-                        if (!SectionMappers.isMapperPresent(reqProductType))
-                            throw new IllegalStateException("Failed to find Section Mapper for class " + reqProductType.getTypeName());
-
-                        //Great we've got the mapper, now we have to find getSection getSections of the setParent one.
-                        //if no getSections are found, or some failed to build, we will provide empty list (null safety)
-                        ConfigurationSection section = configuration.getSection(sectionValue.path());
-
-                        List list = (List) field.get(this);
-
-                        for (ConfigurationSection childSection : section.getSections().values()) {
-
-                            Object mappedObject = SectionMappers.map(childSection, reqProductType);
-                            list.add(mappedObject);
-
-                        }
-
-                    } else {
+                        //Check if getValue exists
+                        if (!section.isPresentValue(configValue.path()))
+                            throw new IllegalStateException("Failed to find configuration getValue at (" + configValue.path() + ") at file (" + section.getConfiguration().getOFile().getFile().getName() + ")");
 
                         reqProductType = field.getType();
-                        if (reqProductType == null) continue;
+                        Object value = section.getValue(configValue.path()).getValue();
 
-                        //Because requirement class is ready to be used
-                        //It's time to look for getSection mapper for required class
-                        if (!SectionMappers.isMapperPresent(reqProductType))
-                            throw new IllegalStateException("Failed to find Section Mapper for class " + reqProductType.getTypeName());
-
-                        //Great we've got the mapper, now we have to find getSection getSections of the setParent one.
-                        //if no getSections are found, or some failed to build, we will provide empty list (null safety)
-                        ConfigurationSection section = configuration.getSection(sectionValue.path());
-
-                        Object mappedObject = SectionMappers.map(section, reqProductType);
+                        //Check if configuration getValue is parsable to field type
+                        if (value.getClass().isInstance(reqProductType))
+                            throw new IllegalStateException("Failed to parse configuration getValue at (" + configValue.path() + ") at file (" + section.getConfiguration().getOFile().getFile().getName() + ") as " + reqProductType.getTypeName());
 
                         //Set the field
-                        field.set(this, mappedObject);
+                        field.set(this, value);
+
+                    } else if (annotation instanceof SectionValue) {
+
+                        SectionValue sectionValue = (SectionValue) annotation;
+
+                        //We have to find out if the getSection at given path even exists
+                        if (!section.isPresentSection(sectionValue.path()))
+                            throw new IllegalStateException("Failed to find configuration getSection at (" + sectionValue.path() + ") at file (" + section.getConfiguration().getOFile().getFile().getName() + ")");
+
+                        //Requires a list of getValues, else it is a single getValue
+                        if (field.getGenericType() instanceof ParameterizedType) {
+
+                            //Check if field getValue is initialized
+                            if (field.get(this) == null)
+                                throw new IllegalStateException("The field list should be initialized in order for configuration to init!");
+
+                            ParameterizedType parameterizedType = (ParameterizedType) field.getGenericType();
+                            for (Type t : parameterizedType.getActualTypeArguments()) {
+
+                                reqProductType = Class.forName(t.getTypeName());
+                                break;
+
+                            }
+
+                            if (reqProductType == null) continue;
+
+                            //Because requirement class is ready to be used
+                            //It's time to look for getSection mapper for required class
+                            if (!SectionMappers.isMapperPresent(reqProductType))
+                                throw new IllegalStateException("Failed to find Section Mapper for class " + reqProductType.getTypeName());
+
+                            //Great we've got the mapper, now we have to find getSection getSections of the setParent one.
+                            //if no getSections are found, or some failed to build, we will provide empty list (null safety)
+                            ConfigurationSection section2 = section.getSection(sectionValue.path());
+
+                            List list = (List) field.get(this);
+
+                            for (ConfigurationSection childSection : section2.getSections().values()) {
+
+                                Object mappedObject = SectionMappers.map(childSection, reqProductType);
+                                list.add(mappedObject);
+
+                            }
+
+                        } else {
+
+                            reqProductType = field.getType();
+                            if (reqProductType == null) continue;
+
+                            //Because requirement class is ready to be used
+                            //It's time to look for getSection mapper for required class
+                            if (!SectionMappers.isMapperPresent(reqProductType))
+                                throw new IllegalStateException("Failed to find Section Mapper for class " + reqProductType.getTypeName());
+
+                            //Great we've got the mapper, now we have to find getSection getSections of the setParent one.
+                            //if no getSections are found, or some failed to build, we will provide empty list (null safety)
+                            ConfigurationSection section2 = section.getSection(sectionValue.path());
+
+                            Object mappedObject = SectionMappers.map(section2, reqProductType);
+
+                            //Set the field
+                            field.set(this, mappedObject);
+
+                        }
 
                     }
 
                 }
 
             }
+        } catch (Exception ex) {
+            getEngine().getLogger().error(new IllegalStateException(ex));
+        }
 
+    }
+
+    default void load(OConfiguration configuration) {
+        try {
+
+            for (Field field : getClass().getDeclaredFields()) {
+
+                field.setAccessible(true);
+
+                for (Annotation annotation : field.getDeclaredAnnotations()) {
+
+                    Class reqProductType = null;
+
+                    if (annotation instanceof ConfigValue) {
+
+                        ConfigValue configValue = (ConfigValue) annotation;
+
+                        //Check if getValue exists
+                        if (!configuration.isPresentValue(configValue.path()))
+                            throw new IllegalStateException("Failed to find configuration getValue at (" + configValue.path() + ") at file (" + configuration.getOFile().getFile().getName() + ")");
+
+                        reqProductType = field.getType();
+                        Object value = configuration.getValue(configValue.path()).getValue();
+
+                        //Check if configuration getValue is parsable to field type
+                        if (value.getClass().isInstance(reqProductType))
+                            throw new IllegalStateException("Failed to parse configuration getValue at (" + configValue.path() + ") at file (" + configuration.getOFile().getFile().getName() + ") as " + reqProductType.getTypeName());
+
+                        //Set the field
+                        field.set(this, value);
+
+                    } else if (annotation instanceof SectionValue) {
+
+                        SectionValue sectionValue = (SectionValue) annotation;
+
+                        //We have to find out if the getSection at given path even exists
+                        if (!configuration.isPresentSection(sectionValue.path()))
+                            throw new IllegalStateException("Failed to find configuration getSection at (" + sectionValue.path() + ") at file (" + configuration.getOFile().getFile().getName() + ")");
+
+                        //Requires a list of getValues, else it is a single getValue
+                        if (field.getGenericType() instanceof ParameterizedType) {
+
+                            //Check if field getValue is initialized
+                            if (field.get(this) == null)
+                                throw new IllegalStateException("The field list should be initialized in order for configuration to init!");
+
+                            ParameterizedType parameterizedType = (ParameterizedType) field.getGenericType();
+                            for (Type t : parameterizedType.getActualTypeArguments()) {
+
+                                reqProductType = Class.forName(t.getTypeName());
+                                break;
+
+                            }
+
+                            if (reqProductType == null) continue;
+
+                            //Because requirement class is ready to be used
+                            //It's time to look for getSection mapper for required class
+                            if (!SectionMappers.isMapperPresent(reqProductType))
+                                throw new IllegalStateException("Failed to find Section Mapper for class " + reqProductType.getTypeName());
+
+                            //Great we've got the mapper, now we have to find getSection getSections of the setParent one.
+                            //if no getSections are found, or some failed to build, we will provide empty list (null safety)
+                            ConfigurationSection section = configuration.getSection(sectionValue.path());
+
+                            List list = (List) field.get(this);
+
+                            for (ConfigurationSection childSection : section.getSections().values()) {
+
+                                Object mappedObject = SectionMappers.map(childSection, reqProductType);
+                                list.add(mappedObject);
+
+                            }
+
+                        } else {
+
+                            reqProductType = field.getType();
+                            if (reqProductType == null) continue;
+
+                            //Because requirement class is ready to be used
+                            //It's time to look for getSection mapper for required class
+                            if (!SectionMappers.isMapperPresent(reqProductType))
+                                throw new IllegalStateException("Failed to find Section Mapper for class " + reqProductType.getTypeName());
+
+                            //Great we've got the mapper, now we have to find getSection getSections of the setParent one.
+                            //if no getSections are found, or some failed to build, we will provide empty list (null safety)
+                            ConfigurationSection section = configuration.getSection(sectionValue.path());
+
+                            Object mappedObject = SectionMappers.map(section, reqProductType);
+
+                            //Set the field
+                            field.set(this, mappedObject);
+
+                        }
+
+                    }
+
+                }
+
+            }
+        } catch (Exception ex) {
+            getEngine().getLogger().error(new IllegalStateException(ex));
         }
 
     }
